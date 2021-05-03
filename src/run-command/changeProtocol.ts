f3d6ff54d88b43c532a86bee7cab62a7ce9d37ac
joe davis <joedavis29@gmail.com>
2021-05-03 19:44:49 -0400

    update protocol

--- src/run-command/changeProtocol.ts
+++ src/run-command/changeProtocol.ts
@@ -2,19 +2,18 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 
-const getReceiverInfo = async (rc) => {
+const changeProtocol = async (rc) => {
   const command = `sudo ir-keytable -p all -s ${rc}`
   const out: string = (await exec(command)).stderr
   if (!rc.includes('Protocols changed')) {
     throw new Error(`
-      ir-keytable not installed,
-      sudo apt-get install ir-keytable -y
+      Error running "${command}" Check this for information to set this 
       https://github.com/gordonturner/ControlKit/blob/master/Raspbian%20Setup%20and%20Configure%20IR.md
     `);
   }
-
-
+  
+  console.log(out)
   return out
 }
 
-export { getReceiverInfo }
+export { changeProtocol }
